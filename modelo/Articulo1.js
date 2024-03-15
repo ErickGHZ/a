c5ddd4a721818ef183720187aeb3991e548dc113
Erick Gutierrez <1722110095@gmail.com>
2024-03-15 11:31:40 -0600

    Modelo

--- modelo/Articulo1.js
+++ modelo/Articulo1.js
@@ -1,1 +1,20 @@
 const {Schema, modelo, trusted} = require('mongoose');
+
+const ArticuloSchema = Schema({
+    titulo:{
+        type: String,
+        required: true,
+    },
+    contenido:{
+        type: String,
+        required: true,
+    },
+    fecha:{
+        type: Date,
+        default: Date.now(),
+    },
+    imagen:{
+        type: String,
+        default:'default.png',
+    },
+})
